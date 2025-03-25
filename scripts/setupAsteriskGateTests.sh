2024adbe1e0610084f1901ba705647a2d8de6dde
George Joseph <gjoseph@sangoma.com>
2025-03-25 12:41:59 -0600

    setupAsteriskGateTests: Remove debugging exits

--- scripts/setupAsteriskGateTests.sh
+++ scripts/setupAsteriskGateTests.sh
@@ -14,14 +14,11 @@ PR_OPTIONS=""
 if [ ${PR_NUMBER} -gt 0 ] ; then
 	PR_OPTIONS="--pr-number=${PR_NUMBER} --is-cherry-pick"
 fi
-exit 0
 
 debug_out "Checking out testsuite"
 ${SCRIPT_DIR}/checkoutRepo.sh --repo="${REPO}" \
 	--repo-dir="${REPO_DIR}" --branch="${BRANCH}" \
 	${PR_OPTIONS} || exit 1
-
-exit 0
 
 echo ${GATETEST_COMMAND} > /tmp/test_commands.json
 TEST_NAME=$(jq -j '.name' /tmp/test_commands.json)
@@ -29,7 +26,6 @@ TEST_OPTIONS=$(jq -j '.options' /tmp/test_commands.json)
 TEST_TIMEOUT=$(jq -j '.timeout' /tmp/test_commands.json)
 TEST_CMD=$(jq -j '.testcmd' /tmp/test_commands.json)
 TEST_DIR=$(jq -j '.dir' /tmp/test_commands.json)
-
 
 export_to_github TEST_NAME TEST_OPTIONS TEST_TIMEOUT TEST_CMD TEST_DIR
 echo "Testsuite setup complete"
