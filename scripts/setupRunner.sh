1e82b96d5889a70e07b5ec295e8269996ff993ed
George Joseph <gjoseph@sangoma.com>
2025-10-13 09:04:52 -0600

    Tidy up setupRunner.sh

--- scripts/setupRunner.sh
+++ scripts/setupRunner.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 FOR_RELEASE=false
-: ${SIPP_VERSION:=v3.6.1}
+: ${SIPP_VERSION:=v3.7.5}
 : ${GITHUB_SERVER_URL:=https://github.com}
 SCRIPT_DIR=$(dirname $(readlink -fn $0))
 . $SCRIPT_DIR/ci.functions
@@ -10,27 +10,27 @@ SCRIPT_DIR=$(dirname $(readlink -fn $0))
 	log_error_msgs "This script must be run as root!"
 	exit 1
 }
+set -e
 
 cat /etc/os-release
+echo "RUNNER_ENVIRONMENT=${RUNNER_ENVIRONMENT}"
 
 export DEBIAN_FRONTEND="noninteractive"
-apt-get update -y -qq >/dev/null
-apt-get install -y -qq wget curl file apt-utils >/dev/null
+debug_out "Running apt update"
+run_silent_unless_error apt-get update -y -qq
+debug_out "Installing wget, curl, file, apr-utils pre-reqs"
+run_silent_unless_error apt-get install -y -qq wget curl file apt-utils
 
-# Install packages
 if ! which gh &>/dev/null ; then
 	debug_out "Installing github cli repo"
 	wget -q -O/tmp/githubcli-archive-keyring.gpg https://cli.github.com/packages/githubcli-archive-keyring.gpg
 	install --mode=0644 -D -t /etc/apt/keyrings /tmp/githubcli-archive-keyring.gpg
-	debug_out "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" > /tmp/github-cli.list
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" > /tmp/github-cli.list
 	install --mode=0644 -D -t /etc/apt/sources.list.d /tmp/github-cli.list
 fi
 
-debug_out "Pre-update cmake version:"
-cmake --version >&2 || :
-
 debug_out "Installing dev packages"
-apt-get install -qq sudo build-essential gdb binutils-dev freetds-dev \
+run_silent_unless_error apt-get install -qq sudo build-essential gdb binutils-dev freetds-dev \
   libasound2-dev libbluetooth-dev libc-client2007e-dev \
   libcap-dev libcfg-dev libcodec2-dev libcorosync-common-dev \
   libcpg-dev libcurl4-openssl-dev libedit-dev libfftw3-dev \
@@ -41,7 +41,7 @@ apt-get install -qq sudo build-essential gdb binutils-dev freetds-dev \
   libspandsp-dev libspeex-dev libspeexdsp-dev libsrtp2-dev \
   libunbound-dev liburiparser-dev libvorbis-dev libxslt1-dev \
   xmlstarlet python3-pystache sqlite3 sqlite3-tools\
-  libsqlite3-dev >/dev/null
+  libsqlite3-dev
 
 addons="cmake libsctp-dev libgsl-dev python3-dev python3*-venv \
   postgresql libpq-dev git libpcap-dev nano python3-pip \
@@ -53,9 +53,9 @@ if ! which jq &>/dev/null ; then
 fi
 
 debug_out "Installing addons" 
-apt-get install -qq ${addons} >/dev/null
+run_silent_unless_error apt-get install -qq ${addons}
 
-if [ -n "$RUNNER_ENVIRONMENT" ] ; then
+if [ -n "${RUNNER_ENVIRONMENT}" ] ; then
 	debug_out "Setting kernel.core_pattern=/tmp/core-%e-%t"
 	sysctl -w kernel.core_pattern=/tmp/core-%e-%t
 	chmod 1777 /tmp
@@ -63,33 +63,28 @@ fi
 
 if $FOR_RELEASE ; then
 	debug_out "Installing release packages"
-	apt-get install -qq python3-markdown python3-markdown-* >/dev/null
+	run_silent_unless_error  apt-get install -qq python3-markdown python3-markdown-*
 	debug_out "Installed release packages.  sipp not needed."
 	exit 0
 fi
 
 debug_out "Removing bison"
-apt-get remove -y -qq bison || :
-apt-get remove -y -qq byacc || :
+run_silent_unless_error apt-get remove -y -qq bison || :
+run_silent_unless_error apt-get remove -y -qq byacc || :
 
 
 debug_out "Building and installing sipp"
 SIPPDIR=$(mktemp -d -p /opt/ -t sipp.XXXXXXXX)
 
 cd ${SIPPDIR}
-debug_out "*** Retrieving sipp ${SIPP_VERSION}"
+debug_out "Retrieving sipp ${SIPP_VERSION}"
 wget -q https://github.com/SIPp/sipp/releases/download/${SIPP_VERSION}/sipp-${SIPP_VERSION/v/}.tar.gz
 tar -xf sipp-${SIPP_VERSION/v/}.tar.gz
 cd sipp-${SIPP_VERSION/v/}
-debug_out "*** Building sipp ${SIPP_VERSION}"
-debug_out "Using cmake version:"
-cmake --version >&2
-cmake . -DUSE_GSL=1 -DUSE_PCAP=1 -DUSE_SSL=1 -DUSE_SCTP=1 -DCMAKE_POLICY_VERSION_MINIMUM=3.5 >/dev/null
-make -j$(nproc --all 2>/dev/null || echo 1) >/dev/null || {
-	log_error_msgs "Failed to build sipp ${SIPP_VERSION}"
-	exit 1
-}
-debug_out "*** Installing sipp ${SIPP_VERSION} to /usr/bin"
-install -D -t /usr/bin sipp
+debug_out "Building sipp ${SIPP_VERSION}"
+run_silent_unless_error cmake . -DUSE_GSL=1 -DUSE_PCAP=1 -DUSE_SSL=1 -DUSE_SCTP=1 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+run_silent_unless_error make -j$(nproc --all 2>/dev/null || echo 1)
+debug_out "Installing sipp ${SIPP_VERSION} to /usr/bin"
+run_silent_unless_error install -D -t /usr/bin sipp
 
 exit 0
