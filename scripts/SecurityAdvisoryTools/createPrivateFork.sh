00f2f674bbe33e14bd5483a65afa61552a50cfb2
George Joseph <gjoseph@sangoma.com>
2026-08-17 12:39:54 -0600

    Updates to allow non-asterisk and testsuite repos to use security stuff

--- scripts/SecurityAdvisoryTools/createPrivateFork.sh
+++ scripts/SecurityAdvisoryTools/createPrivateFork.sh
@@ -37,30 +37,32 @@ if [ -z "${WORK_DIR}" ] && [ -z "${GITHUB_WORKSPACE}" ] ; then
 fi
 
 if [ -z "${SKIP_MASTER_BRANCH_RENAME}" ] ; then
-	SKIP_MASTER_BRANCH_RENAME=true
+	SKIP_MASTER_BRANCH_RENAME=false
 fi
 
 if [ -z "${GITHUB_WORKSPACE}" ] ; then
 	export GITHUB_WORKSPACE=${WORK_DIR}
 fi
 
-
 export GH_TOKEN=${GITHUB_TOKEN}
 export GIT_TOKEN=${GITHUB_TOKEN}
 
 gh auth setup-git -h github.com
 
+json_array_to_array SECURITY_FORK_ACTIONS
+json_array_to_array SECURITY_FIX_BRANCHES
+
+has_actions=false
+if [ ${#SECURITY_FORK_ACTIONS[@]} -gt 0 ] ; then
+	has_actions=true
+fi
+
 REPO_DIR=${GITHUB_WORKSPACE}/$(basename ${DST_REPO})
 echo "Source repository:      asterisk/${SRC_REPO}"
 echo "Local repo directory:   ${REPO_DIR}"
 echo "Destination repository: asterisk/${DST_REPO}"
-IFS=,
-echo -n "Populating branches:    "
-for b in ${SECURITY_FIX_BRANCHES} ; do
-	echo -n "$b "
-done
-echo
-unset IFS
+echo "Enable actions:         ${SECURITY_FORK_ACTIONS[*]:-none}"
+echo "Security fix branches:  ${SECURITY_FIX_BRANCHES[*]:-none}"
 
 echo "Changing directory to ${GITHUB_WORKSPACE}"
 cd "${GITHUB_WORKSPACE}"
@@ -97,34 +99,14 @@ gh repo edit "asterisk/${DST_REPO}" --allow-forking=true --enable-auto-merge=fal
 	--enable-discussions=false --enable-issues=false --enable-merge-commit=false \
 	--enable-wiki=false --enable-projects=false --default-branch=master
 
-echo "Enabling actions on repo asterisk/${DST_REPO}"
-gh api --method PUT \
-	-H "Accept: application/vnd.github+json" \
-	-H "X-GitHub-Api-Version: 2022-11-28" \
-	"/repos/asterisk/${DST_REPO}/actions/permissions" \
-	-F "enabled=true" -f "allowed_actions=all"
-
-if ${SKIP_MASTER_BRANCH_RENAME} ; then
-	# A "GitHub Hack" to enable workflows on the repo.
-	echo "Renaming master branch to main and back again to trigger workflow"
-	gh api --method POST -H "Accept: application/vnd.github+json" \
-		-H "X-GitHub-Api-Version: 2022-11-28" \
-		/repos/asterisk/${DST_REPO}/branches/master/rename -f "new_name=main" || :
-	sleep 2
-	echo "Renaming main branch back to master again to trigger workflow"
-	gh api --method POST -H "Accept: application/vnd.github+json" \
-		-H "X-GitHub-Api-Version: 2022-11-28" \
-		/repos/asterisk/${DST_REPO}/branches/main/rename -f "new_name=master" || :
-fi
-
 # Clone all the labels from the soure repo.
-echo "Copyinglabels from asterisk/${SRC_REPO} to asterisk/${DST_REPO}"
+echo "Copying labels from asterisk/${SRC_REPO} to asterisk/${DST_REPO}"
 gh -R "asterisk/${DST_REPO}" label clone "asterisk/${SRC_REPO}" -f
 
 echo "Pushing branches..."
 cd "./${DST_REPO}"
 IFS=,
-for b in ${SECURITY_FIX_BRANCHES} ; do
+for b in "${SECURITY_FIX_BRANCHES[@]}" ; do
 	if [ "$b" == "master" ] ; then
 		continue
 	fi
@@ -135,6 +117,31 @@ for b in ${SECURITY_FIX_BRANCHES} ; do
 done
 unset IFS
 
+if ! ${has_actions} ; then
+	echo "Actions not used in this repo so no further work needed"
+	exit 0
+fi
+
+echo "Enabling actions on repo asterisk/${DST_REPO}"
+gh api --method PUT \
+	-H "Accept: application/vnd.github+json" \
+	-H "X-GitHub-Api-Version: 2022-11-28" \
+	"/repos/asterisk/${DST_REPO}/actions/permissions" \
+	-F "enabled=true" -f "allowed_actions=all"
+
+if ! ${SKIP_MASTER_BRANCH_RENAME} ; then
+	# A "GitHub Hack" to enable workflows on the repo.
+	echo "Renaming master branch to main and back again to trigger workflow"
+	gh api --method POST -H "Accept: application/vnd.github+json" \
+		-H "X-GitHub-Api-Version: 2022-11-28" \
+		"/repos/asterisk/${DST_REPO}/branches/master/rename" -f "new_name=main" || :
+	sleep 2
+	echo "Renaming main branch back to master again to trigger workflow"
+	gh api --method POST -H "Accept: application/vnd.github+json" \
+		-H "X-GitHub-Api-Version: 2022-11-28" \
+		"/repos/asterisk/${DST_REPO}/branches/main/rename" -f "new_name=master" || :
+fi
+
 # Now that workflows have been enabled, we need yet
 # another "GitHub Hack" to get the workflow files
 # recognized.
@@ -144,7 +151,7 @@ high_branch=$(gh api --paginate \
 	"/repos/asterisk/${DST_REPO}/branches?per_page=100" \
 	--jq '.[] | .name' | grep -E "^[0-9.]+$" | sort -r -V | head -1)
 
-gh repo edit "asterisk/${DST_REPO}" --default-branch=${high_branch}
+gh repo edit "asterisk/${DST_REPO}" --default-branch="${high_branch}"
 sleep 1
 gh repo edit "asterisk/${DST_REPO}" --default-branch=master
 sleep 2
@@ -166,9 +173,14 @@ if [ $wfcount -eq 0 ] ; then
 	echo "$wfcount workflows available after ${elapsed} minutes"
 fi
 
+mapfile -t WORKFLOWS < <(gh api "/repos/asterisk/${DST_REPO}/actions/workflows" --jq '.workflows[].name')
+echo "Disabling all workflows first"
+for wf in "${WORKFLOWS[@]}" ; do
+	gh -R "asterisk/${DST_REPO} workflow" disable "$wf" || :
+done	
+
 # Disable the workflows we never want to run in the private repo.
 echo "Disabling workflows in asterisk/${DST_REPO}"
-declare -a DISABLE_WORKFLOWS=( CreateDocs "Issue Opened" NightlyAdmin NightlyTests Releaser WeeklyTests )
-for w in "${DISABLE_WORKFLOWS[@]}" ; do
-	gh -R asterisk/${DST_REPO} workflow disable "$w" || :
+for wf in "${SECURITY_FORK_ACTIONS[@]}" ; do
+	gh -R "asterisk/${DST_REPO}" workflow enable "$wf" || :
 done
