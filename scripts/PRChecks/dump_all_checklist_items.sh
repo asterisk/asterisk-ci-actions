011086b27b9475d902a9e67dc2301e2b49c686ca
George Joseph <gjoseph@sangoma.com>
2026-08-18 18:46:32 -0600

    Update checklist items with the check script it came from

--- scripts/PRChecks/dump_all_checklist_items.sh
+++ scripts/PRChecks/dump_all_checklist_items.sh
@@ -7,9 +7,14 @@ PR_CHECKLIST_PATH=/dev/stderr
 source ${CHECK_DIR}/checks.functions
 EOF
 
-for f in $(find ${CHECK_DIR} -name '[0-9]*.sh' | sort) ; do
-	sed -n -r -e '/<<[-]?EOF.*print_checklist_item/,/^\s*EOF$/!d' -e 's/^\s*//gp' $f | \
-		sed -r -e 's/>>.*//g' >>/tmp/dump_checklists.sh
+for f in $(find "${CHECK_DIR}" -name '[0-9]*.sh' | sort) ; do
+	check_name=$(basename -s .sh $f)
+	{
+		echo "export CHECK_NAME=${check_name:3}" >>/tmp/dump_checklists.sh
+		echo "echo" >>/tmp/dump_checklists.sh
+		sed -n -r -e '/<<[-]?EOF.*print_checklist_item/,/^\s*EOF$/!d' -e 's/^\s*//gp' $f | \
+			sed -r -e 's/>>.*//g'
+	} >>/tmp/dump_checklists.sh
 done
 chmod a+x /tmp/dump_checklists.sh
 /tmp/dump_checklists.sh
